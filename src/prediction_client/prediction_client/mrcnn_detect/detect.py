58887308d789c6785512fcd2a7c2468aa985a9fd
litwellchi@gmail.com <litwellchi@gmail.com>
2023-03-10 11:52:52 +0800

    construct the detection function

--- src/prediction_client/prediction_client/mrcnn_detect/detect.py
+++ src/prediction_client/prediction_client/mrcnn_detect/detect.py
@@ -7,12 +7,44 @@ import numpy as np
 import cv2
 
 
-config_file = './configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
-time_start = time.time()
-checkpoint_name = 'epoch_2'
-# checkpoint_file  = 'exps_opaque_mix_data/' + checkpoint_name +'.pth'
-checkpoint_file  = '/home/xjgao/InstanceSeg_opaque/exps_opaque/epoch_2_09061103.pth'
+class SuctionPointDetector(object):
+    def __init__(self, args):
+        config_file = './configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
+        checkpoint_file = './epoch_2_09061103.pth'
+        model = init_detector(config_file, checkpoint_file, device='cuda:0')
+        self.result = None
+        pass
 
-# build the model from a config file and a checkpoint file
-model = init_detector(config_file, checkpoint_file, device='cuda:0')
-print("Time consumed model load: ", time.time()-time_start)
+    def detect(self, img):
+        """
+        detect the object on RGB image
+        """
+        result = inference_detector(model, img)
+        print("Time consumed: ", time.time()-time_start)
+        self.result = result
+        return
+
+    def set_box_bound(self, bound):
+        """
+        Set the RGB range of picking box
+        """
+        x_min, x_max, y_min, y_max = 375, 875, 175, 525
+
+    def get_scution_candidate(self, method='simple_midpoint'):
+        """
+        return:pose_list:[[x,y,z,r,p,y],...]
+        """
+        pass
+
+    def show_result(self, save_path=None):
+        """
+        save or show the detection result
+        """
+        model.show_result(self.img, self.result, out_file=save_path)
+        pass
+
+    def __simple_midpoint():
+        pass
+
+    def __depth2pcd():
+        pass
