4ef8d3a39a54eb81bc16adcf479b506f3333bb8e
litwellchi@gmail.com <litwellchi@gmail.com>
2023-03-10 14:28:10 +0800

    implement functions

--- src/prediction_client/prediction_client/mrcnn_detect/detect.py
+++ src/prediction_client/prediction_client/mrcnn_detect/detect.py
@@ -8,21 +8,33 @@ import cv2
 
 
 class SuctionPointDetector(object):
-    def __init__(self, args):
-        config_file = './configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
-        checkpoint_file = './epoch_2_09061103.pth'
-        model = init_detector(config_file, checkpoint_file, device='cuda:0')
+    def __init__(self, config_file, checkpoint_file):
+        self.config_file = config_file
+        self.checkpoint_file = checkpoint_file
+        self.model = init_detector(
+            self.config_file, self.checkpoint_file, device='cuda:0')
         self.result = None
-        pass
+        self.img = None
+        self.depth = None
 
-    def detect(self, img):
+    def find_suction_point(self, img, depth, show_result=False):
+        """
+        Use this function to get the suction point
+        """
+        result = self.instance_detect(img)
+        # masks =
+        suction_candidate = self.get_scution_candidate(masks)
+
+        # if show_result: self.show_result(img,result)
+
+    def instance_detect(self, img):
         """
         detect the object on RGB image
         """
-        result = inference_detector(model, img)
+        time_start = time.time()
+        result = inference_detector(self.model, img)
         print("Time consumed: ", time.time()-time_start)
-        self.result = result
-        return
+        return result
 
     def set_box_bound(self, bound):
         """
@@ -30,21 +42,28 @@ class SuctionPointDetector(object):
         """
         x_min, x_max, y_min, y_max = 375, 875, 175, 525
 
-    def get_scution_candidate(self, method='simple_midpoint'):
+    def get_scution_candidate(self, masks, method='simple_midpoint'):
         """
         return:pose_list:[[x,y,z,r,p,y],...]
         """
-        pass
+        return getattr(self, '__'+method)(masks)
 
-    def show_result(self, save_path=None):
+    def show_result(self, img, result, save_path=None):
         """
         save or show the detection result
         """
-        model.show_result(self.img, self.result, out_file=save_path)
-        pass
+        self.model.show_result(img, result, out_file=save_path)
 
-    def __simple_midpoint():
+    def __simple_midpoint(masks):
+        print("__simple_midpoint")
         pass
 
     def __depth2pcd():
         pass
+
+
+if __name__ == '__main__':
+    # Test code
+    config_file = './configs/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_opaque.py'
+    checkpoint_file = './epoch_2_09061103.pth'
+    SuctionPointDetector(config_file, checkpoint_file)
